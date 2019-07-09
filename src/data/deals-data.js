63cdacbf17b63b4e34a06fcc20bdf8ab3fea97bb
ricojamjar <rich@genericgreeting.co.uk>
2019-07-09 16:26:46 +0100

    changing deals-data from json to js file to allow 'require' functionality

---
+++ src/data/deals-data.js
@@ -0,0 +1,32 @@
+export const dealsObj = {
+  dealsArr: [
+    {
+      price: "£5",
+      drink: "House Lager",
+      quantity: 2,
+      name: "The Pilcrow",
+      venueImg: require("../images/pilcrowIcon.jpg")
+    },
+    {
+      price: "£10",
+      drink: "House Wine",
+      quantity: 1,
+      name: "Terrace",
+      venueImg: require("../images/TerraceIcon.jpg")
+    },
+    {
+      price: "£10",
+      drink: "Mojito",
+      quantity: 2,
+      name: "Trof",
+      venueImg: require("../images/trofIcon.jpg")
+    },
+    {
+      price: "£3",
+      drink: "Schofferhoffer",
+      quantity: 1,
+      name: "Common",
+      venueImg: require("../images/commonIcon.jpg")
+    }
+  ]
+};
