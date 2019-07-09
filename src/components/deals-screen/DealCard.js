0c14dc6e5c68ddb012dcf812d17c483d55186294
ricojamjar <rich@genericgreeting.co.uk>
2019-07-09 16:25:52 +0100

    Adding Venue name and image to deal card

--- src/components/deals-screen/DealCard.js
+++ src/components/deals-screen/DealCard.js
@@ -1,16 +1,19 @@
 import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
-import styled from "styled-components";
 import React from "react";
-import Image from "./CardImage";
-import CardImage from "./CardImage";
+import VenueImage from "./VenueImage";
+import VenueName from "./VenueName";
+import styled from "styled-components";
 
-const Card = styled.View``;
+const Card = styled.View`
+  width: 300;
+`;
 
 export default function DealCard(props) {
-  const {image} = props;
+  const { image, venueName } = props;
   return (
     <Card>
-      <CardImage image={image} />
+      <VenueName name={venueName} />
+      <VenueImage image={image} />
     </Card>
   );
 }
